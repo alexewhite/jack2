3b6d2b56a935b76a923e34efc256c032da7f1b56
Alex <alex.eliz.white@gmail.com>
2025-08-12 09:08:43 -0400

    Initial Commit

---
+++ jack2/jack2App.swift
@@ -0,0 +1,17 @@
+//
+//  jack2App.swift
+//  jack2
+//
+//  Created by Scholar on 8/12/25.
+//
+
+import SwiftUI
+
+@main
+struct jack2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
